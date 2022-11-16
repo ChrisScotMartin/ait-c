c489b91d6b36a812d8c39cc3efe46928b3280faa
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2022-11-16 13:16:58 -0500

    changed pkgmodc from a package to a module

--- pkgmodc/pkgmodc.go
+++ pkgmodc/pkgmodc.go
@@ -1,8 +1,12 @@
-package pkgmodc
+package main
 
 import "fmt"
 
+func main() {
+	fmt.Println("Nothing to see here... move along.")
+}
+
 func Hello() {
 	fmt.Println("From pkgmodc")
-	fmt.Println("Hello, I'm either a package or a module...depends on version.")
+	fmt.Println("Hello, I'm now a module (pkgmodc) at this version.")
 }
