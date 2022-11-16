03d30d017fd3bdc9ed2549b1f8c86fb2994a4456
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2022-11-16 13:01:06 -0500

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChrisScotMartin/ait-c
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ChrisScotMartin/ait-c/pkgmodc"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	pkgmodc.Hello()
+}
+
+func SomeServerFunc() {
+	fmt.Println("I'm a func from the main root module ait-c")
+}
